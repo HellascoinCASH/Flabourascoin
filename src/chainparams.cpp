b0379a31a76fc1a4c9914769698eea96cca20502
Nikolaos Adam <nikospower1999@gmail.com>
2021-05-30 16:19:56 +0200

    Update Ports in Chainparams

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -117,7 +117,7 @@ public:
         pchMessageStart[1] = 0xc0;
         pchMessageStart[2] = 0xb6;
         pchMessageStart[3] = 0xdb;
-        nDefaultPort = 69420;
+        nDefaultPort = 25571;
         nPruneAfterHeight = 100000;
 
         genesis = CreateGenesisBlock(1621957196, 2608067, 0x1e0ffff0, 1, 50 * COIN);
@@ -217,7 +217,7 @@ public:
         pchMessageStart[1] = 0xd2;
         pchMessageStart[2] = 0xc8;
         pchMessageStart[3] = 0xf1;
-        nDefaultPort = 54322;
+        nDefaultPort = 25569;
         nPruneAfterHeight = 1000;
 
         genesis = CreateGenesisBlock(1486949366, 1376642, 0x1e0ffff0, 1, 50 * COIN);
@@ -303,7 +303,7 @@ public:
         pchMessageStart[1] = 0xbf;
         pchMessageStart[2] = 0xb5;
         pchMessageStart[3] = 0xda;
-        nDefaultPort = 19444;
+        nDefaultPort = 25570;
         nPruneAfterHeight = 1000;
 
         genesis = CreateGenesisBlock(1296688602, 0, 0x207fffff, 1, 50 * COIN);
